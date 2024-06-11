9e0bda9c667080c8d4178389cb539891a46c5b5d
kirti sharma <krti401@gmail.com>
2024-06-11 16:57:23 +0530

    chnages

--- owa-frontend-admin/src/app/screens/admin-management/component/add-admin-management/add-admin-management.component.ts
+++ owa-frontend-admin/src/app/screens/admin-management/component/add-admin-management/add-admin-management.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-admin-management',
   templateUrl: './add-admin-management.component.html',
-  styleUrl: './add-admin-management.component.scss'
+  // styleUrl: './add-admin-management.component.scss'
 })
 export class AddAdminManagementComponent {
 
