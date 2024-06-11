1e09690f850d03102597e36184ee8ed8194820ed
kirti sharma <krti401@gmail.com>
2024-06-11 16:07:07 +0530

    changes

--- owa-frontend-user/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'owa-frontend-user',
-  webDir: 'dist'
-};
-
-export default config;
