04fc7e8e7a939bed726e54b8cbed7d7f0c34ac22
Ashwini Jadhav <aj2461995@gmail.com>
2024-06-11 18:35:32 +0530

    admin module added in app module

--- owa-frontend-admin/src/app/app.module.ts
+++ owa-frontend-admin/src/app/app.module.ts
@@ -12,12 +12,13 @@ import { LoaderInterceptor } from './interceptor/loader.interceptor';
 import { ThemeModule } from './modules/shared/theme/theme.module';
 import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
 import { environment } from 'src/environments/environment';
+import { AdminManagementComponent } from './screens/admin-management/admin-management.component';
 import { FormBuilderComponent } from './screens/form-builder/form-builder.component';
 import { WorkflowAssignmentComponent } from './screens/workflow-assignment/workflow-assignment.component';
 import { WorkflowBuilderComponent } from './screens/workflow-builder/workflow-builder.component';
 
 @NgModule({
-  declarations: [AppComponent, FormBuilderComponent, WorkflowAssignmentComponent, WorkflowBuilderComponent,],
+  declarations: [AppComponent, AdminManagementComponent, FormBuilderComponent, WorkflowAssignmentComponent, WorkflowBuilderComponent,],
   imports: [
     BrowserModule,
     AppRoutingModule,
