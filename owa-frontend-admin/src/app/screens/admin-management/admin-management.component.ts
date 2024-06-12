c43ba454847ef4c3fda85929419c8452b7441cbd
Ashwini Jadhav <aj2461995@gmail.com>
2024-06-12 14:08:11 +0530

    admin management ui added

--- owa-frontend-admin/src/app/screens/admin-management/admin-management.component.ts
+++ owa-frontend-admin/src/app/screens/admin-management/admin-management.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-admin-management',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-management.component.scss']
 })
 export class AdminManagementComponent {
+tabData = [
+    { label: 'User Management', content: 'Content 1' },
+    { label: 'Role Management', content: 'Content 2' },
+  ];
+
+  users = [
+    {
+      name: 'Ramesh Thakur',
+      email: 'ramesh@gmail.com',
+      role: 'Super Admin',
+      mobile: '7017019019',
+      image: 'path/to/image1.jpg'
+    },
+    {
+      name: 'Kamal Kishor',
+      email: 'kamal@gmail.com',
+      role: 'Admin',
+      mobile: '7017019019',
+      image: 'path/to/image2.jpg'
+    },
+    {
+      name: 'Nidhi Chandel',
+      email: 'nidhi@gmail.com',
+      role: 'Contributor',
+      mobile: '7017019019',
+      image: 'path/to/image3.jpg'
+    },
+    {
+      name: 'Samarth Gupta',
+      email: 'samarth@gmail.com',
+      role: 'Super Admin',
+      mobile: '7017019019',
+      image: 'path/to/image4.jpg'
+    },
+    {
+      name: 'Ashok Sharma',
+      email: 'ashok@gmail.com',
+      role: 'Super Admin',
+      mobile: '7017019019',
+      image: 'path/to/image5.jpg'
+    }
+  ];
+
+  getRoleClass(role: string) {
+    switch (role) {
+      case 'Super Admin':
+        return 'bg-blue-500';
+      case 'Admin':
+        return 'bg-blue-400';
+      case 'Contributor':
+        return 'bg-blue-300';
+      default:
+        return 'bg-gray-500';
+    }
+  }
 
 }
