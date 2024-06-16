7cdac58c8e3c9f37b34dd669d425431c4ed8adb0
sudhir-orrizonte <sudhir.allada@orrizonte.in>
2024-06-16 21:32:26 +0530

    Swagger fix

--- owa-micro-forms/config/swagger.ts
+++ owa-micro-forms/config/swagger.ts
@@ -5,15 +5,18 @@ import { envUtils } from "owa-micro-common";
 const moduleName = envUtils.getStringEnvVariableOrDefault("MODULE", "owa-micro-forms");
 const port = envUtils.getNumberEnvVariableOrDefault("PORT", 9005);
 const apiBaseUrl = envUtils.getStringEnvVariableOrDefault("OWA_APIS_BASE_URL", `localhost:${port}`);
+const scheme = apiBaseUrl.includes("localhost") ? "http" : "https";
 
 const doc = {
     info: {
         title: moduleName,
         description: `API Swagger for ${moduleName}`,
     },
-    host: apiBaseUrl,
-    basePath: '/',
-    schemes: [apiBaseUrl.includes("localhost") ? "http" : "https"]
+    servers: [
+        {
+          url: `${scheme}://${apiBaseUrl}`            
+        }
+    ]
 };
 
 const outputFile = './swagger.json';
