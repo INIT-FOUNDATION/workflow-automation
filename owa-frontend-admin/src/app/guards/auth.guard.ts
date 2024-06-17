51f9ac9dc757134949b8b156079a95c11bf0e855
skm-orrizonte <sachin.mogaveera@orrizonte.in>
2024-06-18 00:39:06 +0530

    changes

--- owa-frontend-admin/src/app/guards/auth.guard.ts
+++ owa-frontend-admin/src/app/guards/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { AuthService } from '../screens/auth/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,26 +7,26 @@ import { AuthService } from '../screens/auth/services/auth.service';
 
 export class AuthGuard implements CanActivate {
   constructor(private router: Router,
-    private auth: AuthService,
   ) {
   }
   exclude_urls = ['/login'];
   urls_can_visit_bfr_login = ['/login', '/home'];
-  routes_navigation_without_login = ['/login', '/hospital-boarding']
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-    if (this.auth.currentUserValue) {
-      if (this.routes_navigation_without_login.indexOf(state.url) != -1) {
-        //this.auth.redirectAsperPermission();
-      }
-      return true;
-    } else {
-      if (this.routes_navigation_without_login.indexOf(state.url) == -1) {
-        this.router.navigate(['/login']);
-      }
-      return true;
-    }
+  
+    return true;
   }
 
+  // if (this.auth.currentUserValue) {
+  //   if (this.routes_navigation_without_login.indexOf(state.url) != -1) {
+  //     //this.auth.redirectAsperPermission();
+  //   }
+  //   return true;
+  // } else {
+  //   if (this.routes_navigation_without_login.indexOf(state.url) == -1) {
+  //     this.router.navigate(['/login']);
+  //   }
+  //   return true;
+  // }
 }
