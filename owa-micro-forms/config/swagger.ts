57509f5becf388796715df74074bb877b1c4f158
sudhir-orrizonte <sudhir.allada@orrizonte.in>
2024-06-17 17:27:30 +0530

    Swagger changed to open api 3.0.0

--- owa-micro-forms/config/swagger.ts
+++ owa-micro-forms/config/swagger.ts
@@ -6,15 +6,72 @@ const apiBaseUrl = process.env.OWA_APIS_BASE_URL || `localhost:${port}`;
 const scheme = apiBaseUrl.includes("localhost") ? "http" : "https";
 const doc = {
     info: {
+        version: '1.0.0',
         title: moduleName,
         description: `API Swagger for ${moduleName}`,
     },
-    basePath: "/",
-    host: apiBaseUrl,
-    schemes: [scheme]
+    servers: [
+        {
+            url: 'http://localhost:9005',
+            description: 'Local Server'
+        },
+        {
+            url: 'https://apiowa.dev.orrizonte.in',
+            description: 'Development Server'
+        },
+        {
+            url: 'https://apiowa.orrizonte.in',
+            description: 'Production Server'
+        }
+    ],
+    components: {
+        securitySchemes: {
+          apiKeyAuth: {
+            type: 'apiKey',
+            in: 'header',
+            name: 'Authorization',
+            description: 'Enter your token',
+          },
+        },
+        security: [{
+            apiKeyAuth: []
+        }],
+        schemas: {
+            formGridSchema: {
+                $page_size: 50,
+                $current_page: 1,
+                search_query: ''
+            },
+            updateFormStatusSchema: {
+                $form_id: 7,
+                $status: 1
+            },
+            createFormSchema: {
+                form_name: "School Survey Form 2",
+                form_description: "This form is used for taking feedback from schools cxzczx",
+                form_fields: [
+                    {
+                        field_id: 1,
+                        options: {}
+                    }
+                ]
+            },
+            updateFormSchema: {
+                form_id: 7,
+                form_name: "School Survey Form 2",
+                form_description: "This form is used for taking feedback from schools cxzczx",
+                form_fields: [
+                    {
+                        field_id: 1,
+                        options: {}
+                    }
+                ]
+            }
+        }
+    },
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['../startup/routes.ts'];
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen({openapi: '3.0.0'})(outputFile, endpointsFiles, doc);
