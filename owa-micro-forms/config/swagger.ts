e9b48d6f4b6036bed0b4b8197852fb80ab70ba56
Narsima Chilkuri <168176028+narsima-orrizonte@users.noreply.github.com>
2024-06-17 14:00:31 +0530

    swagger fix for forms service

--- owa-micro-forms/config/swagger.ts
+++ owa-micro-forms/config/swagger.ts
@@ -4,28 +4,17 @@ const moduleName = process.env.MODULE || "owa-micro-forms"
 const port = process.env.PORT || 9005;
 const apiBaseUrl = process.env.OWA_APIS_BASE_URL || `localhost:${port}`;
 const scheme = apiBaseUrl.includes("localhost") ? "http" : "https";
-
 const doc = {
     info: {
         title: moduleName,
         description: `API Swagger for ${moduleName}`,
     },
-    securityDefinitions: {
-        Bearer: {
-          type: 'apiKey',
-          name: 'Authorization',
-          in: 'header',
-          description: 'Enter your bearer token in the format **&lt;token&gt;**'
-        }
-    },
-    servers: [
-        {
-          url: `${scheme}://${apiBaseUrl}`            
-        }
-      ]
+    basePath: "/",
+    host: apiBaseUrl,
+    schemes: [scheme]
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['../startup/routes.ts'];
 
-swaggerAutogen({openapi: '3.0.0'})(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc);
