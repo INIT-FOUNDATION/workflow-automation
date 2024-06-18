28552f6db3f7dab6c5acd549f5bc1c83c9aa9b45
shubham-orrizonte <shubham.baranwal@orrizonte.in>
2024-06-18 18:11:37 +0530

    Integrated sidebar UI design

--- owa-frontend-admin/src/app/modules/shared/components/sidebar-menu/sidebar-menu.component.ts
+++ owa-frontend-admin/src/app/modules/shared/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-menu',
@@ -8,32 +15,37 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   styleUrls: ['./sidebar-menu.component.scss'],
   animations: [
     trigger('toggleSidebar', [
-      state('open', style({
-        transform: 'translateX(0)'
-      })),
-      state('closed', style({
-        transform: 'translateX(-100%)'
-      })),
-      transition('open <=> closed', [
-        animate('0.3s ease-in-out')
-      ])
-    ])
-  ]
+      state(
+        'open',
+        style({
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          transform: 'translateX(-100%)',
+        })
+      ),
+      transition('open <=> closed', [animate('0.3s ease-in-out')]),
+    ]),
+  ],
 })
 export class SidebarMenuComponent implements OnInit {
+  selectedTab: number = 1;
   menu_items = [];
   isOpen = false;
-
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private router: Router) {}
 
   ngOnInit(): void {
     this.prepareMenuItems();
+    console.log(this.router.url);
   }
 
   prepareMenuItems() {
     this.dataService.permissionPresent$.subscribe((res) => {
       if (res) {
-        this.menu_items = res.map(value => ({
+        this.menu_items = res.map((value) => ({
           label: value.menu_name,
           route_url: value.route_url,
           icon: value.icon_class,
@@ -46,5 +58,9 @@ export class SidebarMenuComponent implements OnInit {
 
   toggleMenu() {
     this.isOpen = !this.isOpen;
+  }
+
+  activeTab(value) {
+    this.selectedTab = value;
   }
 }
