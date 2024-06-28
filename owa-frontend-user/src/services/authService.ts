81c1bc075a7968c4c74601277aca21aaabf5d929
harshita-orrizonte <harshita.garg@orrizonte.in>
2024-06-28 21:41:06 +0530

    Enhance AuthContext with token management and user details retrieval

--- owa-frontend-user/src/services/authService.ts
+++ owa-frontend-user/src/services/authService.ts
@@ -1,4 +1,4 @@
-import { post } from "../utility/ApiCall";
+import { get, post } from "../utility/ApiCall";
 
 const loginWithMobile = async (payload: any) => {
   const loginByMobile = await post("/api/v1/auth/student/getOtp", payload);
@@ -14,22 +14,49 @@ const verifyMobileOtp = async (payload: any) => {
 };
 
 const loginPassword = async (payload: any) => {
-    const loginByMobileRes = await post("/api/v1/auth/admin/login", payload);
-    return loginByMobileRes;
-  };
+  const loginByMobileRes = await post("/api/v1/auth/admin/login", payload);
+  return loginByMobileRes;
+};
 
-  const otpRequest=async(payload: any)=>{
-    const getOtpRequest=await post("/api/v1/admin/getForgetPasswordOtp",payload);
-    return getOtpRequest;
-  }
-  const verifyOtpRequest=async(payload: any)=>{
-      const getVerifyOtpRequest=await post("/api/v1/admin/verifyForgetPasswordOtp",payload);
-    return getVerifyOtpRequest;
-  }
-  const ResetPasswordRequest=async(payload: any)=>{
-    const getResetPasswordRequest=await post("/api/v1/admin/resetForgetPassword",payload);
-    return getResetPasswordRequest;
-  }
-   
+const otpRequest = async (payload: any) => {
+  const getOtpRequest = await post(
+    "/api/v1/admin/getForgetPasswordOtp",
+    payload
+  );
+  return getOtpRequest;
+};
+const verifyOtpRequest = async (payload: any) => {
+  const getVerifyOtpRequest = await post(
+    "/api/v1/admin/verifyForgetPasswordOtp",
+    payload
+  );
+  return getVerifyOtpRequest;
+};
+const resetPasswordRequest = async (payload: any) => {
+  const getResetPasswordRequest = await post(
+    "/api/v1/admin/resetForgetPassword",
+    payload
+  );
+  return getResetPasswordRequest;
+};
 
-export { loginWithMobile, verifyMobileOtp, loginPassword,otpRequest, verifyOtpRequest, ResetPasswordRequest };
+const getLoggedInUserDetails = async (addUserDetailsToContext: Function) => {
+  try {
+    const getLoginUserInfo = await get("/api/v1/user/admin/loggedUserInfo");
+    const loggedInUser = getLoginUserInfo?.data;
+    addUserDetailsToContext(loggedInUser);
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
+export {
+  loginWithMobile,
+  verifyMobileOtp,
+  loginPassword,
+  otpRequest,
+  verifyOtpRequest,
+  resetPasswordRequest,
+  getLoggedInUserDetails,
+};
