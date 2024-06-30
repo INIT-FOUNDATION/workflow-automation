27499d523f596204104bebc222c5b900738cef78
sudhir-orrizonte <sudhir.allada@orrizonte.in>
2024-07-01 03:15:10 +0530

    Issue resolved drawflow

--- owa-frontend-admin/src/app/screens/workflow-builder/workflow-builder.component.ts
+++ owa-frontend-admin/src/app/screens/workflow-builder/workflow-builder.component.ts
@@ -33,9 +33,9 @@ export class WorkflowBuilderComponent implements AfterViewInit {
   mobile_item_selec: string;
   mobile_last_move: TouchEvent | null;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   ngAfterViewInit(): void {
     this.initDrawingBoard();
@@ -66,70 +66,12 @@ export class WorkflowBuilderComponent implements AfterViewInit {
 
       const dataToImport = {
         drawflow: {
+          Home: {
+            data: {
+            }
+          },
           Other: {
             data: {
-              '8': {
-                id: 8,
-                name: 'personalized',
-                data: {},
-                class: 'personalized',
-                html: '\n            <div>\n              Personalized\n            </div>\n            ',
-                typenode: false,
-                inputs: {
-                  input_1: {
-                    connections: [
-                      { node: '12', input: 'output_1' },
-                      { node: '12', input: 'output_2' },
-                      { node: '12', input: 'output_3' },
-                      { node: '12', input: 'output_4' },
-                    ],
-                  },
-                },
-                outputs: {
-                  output_1: { connections: [{ node: '9', output: 'input_1' }] },
-                },
-                pos_x: 764,
-                pos_y: 227,
-              },
-              '9': {
-                id: 9,
-                name: 'dbclick',
-                data: { name: 'Hello World!!' },
-                class: 'dbclick',
-                html: '\n            <div>\n            <div class="title-box"><i class="fas fa-mouse"></i> Db Click</div>\n              <div class="box dbclickbox" ondblclick="showpopup(event)">\n                Db Click here\n                <div class="modal" style="display:none">\n                  <div class="modal-content">\n                    <span class="close" onclick="closemodal(event)">&times;</span>\n                    Change your variable {name} !\n                    <input type="text" df-name>\n                  </div>\n\n                </div>\n              </div>\n            </div>\n            ',
-                typenode: false,
-                inputs: {
-                  input_1: { connections: [{ node: '8', input: 'output_1' }] },
-                },
-                outputs: {
-                  output_1: {
-                    connections: [{ node: '12', output: 'input_2' }],
-                  },
-                },
-                pos_x: 209,
-                pos_y: 38,
-              },
-              '12': {
-                id: 12,
-                name: 'multiple',
-                data: {},
-                class: 'multiple',
-                html: '\n            <div>\n              <div class="box">\n                Multiple!\n              </div>\n            </div>\n            ',
-                typenode: false,
-                inputs: {
-                  input_1: { connections: [] },
-                  input_2: { connections: [{ node: '9', input: 'output_1' }] },
-                  input_3: { connections: [] },
-                },
-                outputs: {
-                  output_1: { connections: [{ node: '8', output: 'input_1' }] },
-                  output_2: { connections: [{ node: '8', output: 'input_1' }] },
-                  output_3: { connections: [{ node: '8', output: 'input_1' }] },
-                  output_4: { connections: [{ node: '8', output: 'input_1' }] },
-                },
-                pos_x: 179,
-                pos_y: 272,
-              },
             },
           },
         },
@@ -182,7 +124,7 @@ export class WorkflowBuilderComponent implements AfterViewInit {
         }
         this.selectedNode =
           this.editor.drawflow.drawflow.Home.data[
-            `${this.selectedNodeId.slice(5)}`
+          `${this.selectedNodeId.slice(5)}`
           ];
       }
 
@@ -253,7 +195,7 @@ export class WorkflowBuilderComponent implements AfterViewInit {
         false
       );
       element.addEventListener('touchstart', this.drag.bind(this), false);
-      element.addEventListener('dblclick', (event) => {});
+      element.addEventListener('dblclick', (event) => { });
     });
   }
 
@@ -308,18 +250,18 @@ export class WorkflowBuilderComponent implements AfterViewInit {
 
     pos_x =
       pos_x *
-        (this.editor.precanvas.clientWidth /
-          (this.editor.precanvas.clientWidth * this.editor.zoom)) -
+      (this.editor.precanvas.clientWidth /
+        (this.editor.precanvas.clientWidth * this.editor.zoom)) -
       this.editor.precanvas.getBoundingClientRect().x *
-        (this.editor.precanvas.clientWidth /
-          (this.editor.precanvas.clientWidth * this.editor.zoom));
+      (this.editor.precanvas.clientWidth /
+        (this.editor.precanvas.clientWidth * this.editor.zoom));
     pos_y =
       pos_y *
-        (this.editor.precanvas.clientHeight /
-          (this.editor.precanvas.clientHeight * this.editor.zoom)) -
+      (this.editor.precanvas.clientHeight /
+        (this.editor.precanvas.clientHeight * this.editor.zoom)) -
       this.editor.precanvas.getBoundingClientRect().y *
-        (this.editor.precanvas.clientHeight /
-          (this.editor.precanvas.clientHeight * this.editor.zoom));
+      (this.editor.precanvas.clientHeight /
+        (this.editor.precanvas.clientHeight * this.editor.zoom));
 
     switch (name) {
       case 'facebook':
