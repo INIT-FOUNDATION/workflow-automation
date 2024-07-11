45145a2328e198bd01af2f03f79d924028f5e39d
ketan-orrizonte <ketan.tawade@orrizonte.in>
2024-07-11 21:39:29 +0530

    form_id made optional

--- owa-micro-workflows/models/workflowModel.ts
+++ owa-micro-workflows/models/workflowModel.ts
@@ -81,7 +81,7 @@ class WorkflowTask implements IWorkflowTask {
             task_description: Joi.string().optional().allow(null).error(
                 new Error(`${WORKFLOWS.WORKF0002}`)
             ),
-            form_id: Joi.number().integer().required(),
+            form_id: Joi.number().integer().optional(),
             status: Joi.number().integer().default(1),
             x_axis: Joi.string().required(),
             y_axis: Joi.string().required(),
